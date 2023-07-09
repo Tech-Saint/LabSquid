62697049d40a65d39b6b17a11cf585e62c496aeb
Thomas <Tomlewho@gmail.com>
2023-07-08 22:14:59 -0400

    Added Bootstrap to Ui

--- App.py
+++ App.py
@@ -3,17 +3,18 @@ from json import dumps as jsonstring
 
 
 from flask import Flask,render_template,request
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+#Bootstrap(app)
 
 @app.route('/', methods=['GET', 'POST'])
+
 def index():
     if request.method == 'POST':
         if request.form.get('action1') == 'VALUE1':
             DeviceInstances,session=init_backend()
-            return render_template('devices.html', devices=DeviceInstances) # do something
-        elif  request.form.get('action2') == 'VALUE2':
-            pass # do something else
+            return render_template('devices.html', devices=DeviceInstances)
         else:
             pass # unknown
     elif request.method == 'GET':
