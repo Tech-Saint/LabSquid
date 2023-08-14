0e6265118c9ca77bd38be3e43b0331d29abfbe6a
Thomas <Tomlewho@gmail.com>
2023-08-14 04:37:01 -0400

    Moved imports to prevent cyclic imports

--- Webinterface/__init__.py
+++ Webinterface/__init__.py
@@ -1,19 +1,23 @@
 from flask import Flask
 from Labmanager.labmanager import init_backend
-from .views import views
-from .devices import devices
-from .auth import auth
+
+session=""
 
 def Main_app():
     app = Flask(__name__)
 
 
     with app.app_context():
+        global session
         session = init_backend()
     @app.context_processor
     def inject_global_vars():
 
         return {'lab': session}  # Return the session object as a dictionary
+        
+    from .views import views
+    from .devices import devices
+    from .auth import auth
 
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(devices, url_prefix="/")
