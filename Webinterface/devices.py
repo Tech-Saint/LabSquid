f9fa7b5b1afe9e41e4360ba4c0fdc3ef996c3908
Thomas <Tomlewho@gmail.com>
2023-08-13 22:33:37 -0400

    Added Device routes

--- Webinterface/devices.py
+++ Webinterface/devices.py
@@ -7,4 +7,15 @@ devices = Blueprint('devices', __name__)
 @devices.route('/devices')
 def showdevices():
 
-    return render_template('devices.html' )
+    return render_template('Alldevices.html' )
+
+@devices.route('/devices/<Device>')
+def sh_single_dev(Device):
+
+    return render_template('Device.html',device=Device)
+
+@devices.route('/add/device')
+def addDevice():
+
+    return render_template('Adddevice.html',)
+
