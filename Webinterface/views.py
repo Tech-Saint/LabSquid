83090ab392bf152c77f2a78f19cfc3a80ac8e5c9
Thomas Linkey Jr <tomlewho@gmail.com>
2023-08-21 21:26:05 -0400

    Added new calls and removed unneeded import

--- Webinterface/views.py
+++ Webinterface/views.py
@@ -4,7 +4,7 @@ from json import dumps as jsonstring
 
 from flask import Flask,render_template,request,Blueprint
 
-from flask_bootstrap import Bootstrap
+from Webinterface import session
 
 views = Blueprint('views', __name__)
 
@@ -12,3 +12,10 @@ views = Blueprint('views', __name__)
 def home():
     return render_template('index.html')
 
+@views.route('Settings',methods=['GET', 'POST'])
+def settings():
+    return render_template('settings.html')
+
+@views.get("/uptime")
+def return_uptime():
+    return session.uptime()
