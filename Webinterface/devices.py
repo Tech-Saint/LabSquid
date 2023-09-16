184c68923165c58f7e5893f55772aeacc04a79e1
Thomas <Tomlewho@gmail.com>
2023-09-16 16:09:33 -0400

    Added working Device edit page

--- Webinterface/devices.py
+++ Webinterface/devices.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template , request, flash, redirect, url_for, session
-
-import re
+from concurrent.futures import as_completed,ThreadPoolExecutor
+import re, copy
 from Labmanager.bin.clients import init_device_objs
 
 
@@ -14,6 +14,54 @@ def showdevices():
     if 'logged_in' not in session:
         return redirect("/login")
     return render_template('Alldevices.html' )
+
+@devices.route('/device/<device>/edit',methods=('GET', 'POST'))
+def edit_single_dev(device):
+    try:
+        if request.method == "POST":
+            if lab.db.unsaved_changes != True:
+                #append data 
+                temp_device = copy.deepcopy(lab.db.data['devices'][lab.DeviceInstances[device].device["id"]])
+                for i in request.form:
+                    if request.form[i] == '':
+                        continue
+                    else:
+                        temp_device[i]=request.form[i]
+                #check data 
+                result = lab.db.Verify_data(temp_data=temp_device)
+                if len(result) != 0:
+                    for i in result:
+                        flash(f"{i.capitalize()}","danger")
+                    return render_template('EditDevice.html',device=device)
+                
+                else:
+                    #apply changes 
+                    lab.db.temp_data['devices'][lab.DeviceInstances[device].device["id"]]=temp_device
+                    result=lab.db.save_db()
+                    lab.db.load_db()
+                    with ThreadPoolExecutor(max_workers=1) as executor:
+                        init_device = executor.submit(init_device_objs,temp_device)
+
+                        try:
+                            lab.DeviceInstances[device] = init_device.result()
+                            print("re-initialized: "+ device)
+                        except Exception as e:
+                            print(f"Error: {e}")
+
+                    # Update device IDs
+                    for _ in lab.DeviceInstances:
+                        a=next(( item for item in lab.db.data['devices'] if item["DNS_name"] == _),None)
+                        lab.DeviceInstances[_].device['id']= int(a["id"])
+                        
+
+                    return redirect(f'/device/{device}')
+    
+    except Exception as e:
+        print(e)
+
+    return render_template('EditDevice.html',device=device)
+
+
 
 @devices.route('/device/<Device>',methods=('GET', 'POST'))
 def sh_single_dev(Device):
@@ -56,41 +104,30 @@ def addDevice():
             if request.form["device_type"]=="Device Type":
                 flash("Please select a device type")
                 formInvalid=True
-            ip_octet_regex=r"(?:[\d]{1,3})"
-            ip_format_regex=r"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}"
-            highest_value=257
-
+            
             if formInvalid==True:return render_template('Adddevice.html',)
+            try: 
+                lab.DeviceInstances[request.form["DNS_name"]]
+                flash(f"The DNS name: '{request.form['DNS_name']}' already exists!")
+                return render_template('Adddevice.html')      
+            except KeyError: pass
+            new_device=dict(request.form)
+            new_device["password"]=new_device["password1"]
 
-            if re.match(ip_format_regex,request.form["ip"]) != None:  
-                try:
-                    highest_value=max([int(i) for i in re.findall(ip_octet_regex, request.form["ip"])])
-                    if highest_value > 254:
-                            raise Exception
-                    
-                    else:
-                        try: 
-                            lab.DeviceInstances[request.form["DNS_name"]]
-                            flash(f"The DNS name: '{request.form['DNS_name']}' already exists!")
-                            return render_template('Adddevice.html')
+            id = max([id['id'] for id in lab.db.temp_data['devices']])
+            id+=1
+            new_device["id"]=id
+            del new_device["password2"]
+            del new_device["password1"]
+            lab.db.add_device(new_device)
+            lab.db.save_db()
+            lab.db.load_db()       
+ 
 
-                        except KeyError: pass
-                        new_device=dict(request.form)
-                        new_device["password"]=new_device["password1"]
-                        del new_device["password2"]
-                        del new_device["password1"]
-                        lab.db.add_device(new_device)
-                        lab.db.load_db()
-                        
-                        lab.DeviceInstances[new_device["DNS_name"]]=init_device_objs(new_device)
-                        return redirect(f"/device/{new_device['DNS_name']}")
+            lab.DeviceInstances[new_device["DNS_name"]]=init_device_objs(new_device)
+            return redirect(f"/device/{new_device['DNS_name']}")
 
-                except Exception as e: 
-                    flash(f"{e}!")
-
-            else: 
-                flash("IP address is invalid!")
     except Exception as e:
-        print(e)
-    return render_template('Adddevice.html',)
+        flash(e)
+    return render_template('Adddevice.html',device)
 
