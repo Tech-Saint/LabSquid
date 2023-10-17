08fafabfd1c48e5b5e7b91a449c7438a5eba079b
Thomas <Tomlewho@gmail.com>
2023-10-17 19:14:16 -0400

    Modfied host batt funct

--- Labmanager/src/roles/BattHost.py
+++ Labmanager/src/roles/BattHost.py
@@ -1,6 +1,6 @@
 import time, subprocess, re
 
-def Batt_check(self)-> int:
+def Host_Batt_check(self)-> int:
     processR= subprocess.run(['apcaccess -p BCHARGE'],shell=True, check=True, stdout=subprocess.PIPE).stdout.decode('utf-8')
     time.sleep(5)
     x = re.findall('\d{1,3}',processR)
