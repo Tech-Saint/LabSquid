8e375d261244705f51e2b0ca1f7f7d176e985afc
Thomas Linkey <tomlewho@gmail.com>
2024-01-23 15:46:56 -0500

    Added dummy distro through ubuntu

--- Labmanager/src/roles/Linux_distro.py
+++ Labmanager/src/roles/Linux_distro.py
@@ -1,1 +1,22 @@
 
+from ..client_base import linux
+
+class ubuntu(linux):
+    deviceName = "ubuntu"
+    def __init__(self, device:str):
+        super().__init__(device)
+
+        self.commands = {
+            "shutdown":["sudo shutdown"],
+                "reboot":["sudo reboot"]
+        }
+
+
+    def update_info(self):
+        """This updates the class instance's copy of the data base for its respective entry.
+        
+        Note: this SHOULD not update the main session.db.data. 
+        """
+
+        self.ip
+        return "Updated"
